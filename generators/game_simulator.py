0a7661168def6a98929b7897bb4e98fd8ab9e334
fatconan <Ian.Usher@deltadna.com>
2015-03-16 15:34:24 +0000

    Adding work on the simulator and fixture reader. It's currently super slow

---
+++ generators/game_simulator.py
@@ -0,0 +1,68 @@
+import random
+
+class GameSimulator(object):
+    default_outcomes = [(('Win', 2, None), ('Loss', 0, None)), (('Win OT', 2, None),('Loss OT', 1, None)), (('Win SO', 2, None),('Loss SO', 1, None)), (('Win', 2, None), ('Loss', 0, None))]
+
+    @classmethod
+    def simulate(cls, teams, outcomes):
+        outcome = random.choice(outcomes)
+
+        order = [0, 1]
+        random.shuffle(order)
+        primary = teams[order[0]]
+        secondary = teams[order[1]]
+
+        primary.add_outcome(outcome[0])
+        secondary.add_outcome(outcome[1])
+
+class Team(object):
+    def __init__(self, name):
+        self.name = name
+        self.outcomes = []
+        self.points = 0
+
+    def add_outcome(self, outcome):
+        self.outcomes.append(outcome)
+        self.points += outcome[1]
+
+    def reset(self):
+        self.points = 0
+        self.outcomes = []
+
+class League(object):
+    def __init__(self):
+        self.teams = {}
+        self.fixtures = []
+
+    def reset(self):
+        for t in self.teams.values():
+            t.reset()
+
+    def add_team(self, team):
+        if self.teams.get(team.name) is None:
+            self.teams[team.name] = team
+
+    def add_fixture(self, fixture):
+        self.fixtures.append(fixture)
+
+    def play_games(self):
+        for team_name_a, team_name_b, already_played in self.fixtures:
+            if not already_played:
+                GameSimulator.simulate((self.teams.get(team_name_a), self.teams.get(team_name_b)), GameSimulator.default_outcomes)
+            else:
+                self.teams.get(team_name_a).add_outcome(already_played[0])
+                self.teams.get(team_name_b).add_outcome(already_played[1])
+
+        league_final = self.teams.values()
+        league_final.sort(key=lambda x: -x.points)
+        return league_final
+
+
+if __name__ == '__main__':
+    teams = [Team('Team A'), Team('Team B')]
+
+    for i in range(0, 52):
+        GameSimulator.simulate(teams, GameSimulator.default_outcomes)
+
+    for t in teams:
+        print t.name, t.points, t.outcomes
---
+++ scraper/fixture_scraper.py
@@ -0,0 +1,115 @@
+#coding: utf-8
+import json
+import os
+import sys
+sys.path.extend(['C:\\Users\\Fat\\Dropbox\\eliteHockey'])
+
+from bs4 import BeautifulSoup
+import requests
+import time
+from generators.game_simulator import League, Team, GameSimulator
+from helpers.progress import Progress
+
+
+class Scraper(object):
+    @classmethod
+    def scrape(cls):
+        response = requests.get('http://www.eliteleague.co.uk/fixtures-s12347')
+        soup = BeautifulSoup(response.text)
+
+        try:
+            table = soup.select("div.games table.rosterTable")[0]
+            table_text = unicode(table)
+        except IndexError:
+            table_text = None
+
+        results = []
+        if table_text is not None:
+            table = BeautifulSoup(table_text)
+            rows = table("tr")
+
+            header = {}
+            for i, row in enumerate(rows):
+                if row("th"):
+                    tds = row("th")
+                    header = dict([(t.get_text(), i) for i, t in enumerate(tds)])
+                elif row("td"):
+                    if i % 2== 0:
+                        tds = row("td")
+                        if 'League' in tds[header.get('Game ID')].get_text():
+                            data = [tds[header.get('Date')], tds[header.get('Visiting Team')], tds[header.get('Home Team')], tds[header.get('Game ID')], tds[header.get('Score')]]
+                            results.append([t.get_text() for t in data])
+
+            with open(os.path.join(os.path.abspath("../resources"),"table.json"), "wb") as json_file:
+                json.dump(results, json_file)
+
+    @classmethod
+    def read_stored_json(cls):
+         with open(os.path.join(os.path.abspath("../resources"),"table.json"), "rb") as json_file:
+                return json.load(json_file)
+
+if __name__ == '__main__':
+
+    Scraper.scrape()
+    results = Scraper.read_stored_json()
+    league = League()
+
+    for result in results:
+        teamA = Team(result[1])
+        teamB = Team(result[2])
+        league.add_team(teamA)
+        league.add_team(teamB)
+
+        assigned_outcome = None
+        if result[4]:
+            if "SO" in result[4]:
+                outcome = GameSimulator.default_outcomes[2]
+            elif "OT" in result[4]:
+                outcome = GameSimulator.default_outcomes[1]
+            else:
+                outcome = GameSimulator.default_outcomes[1]
+            score_text = unicode(result[4])
+            scores = [int(score) for score in score_text.replace(u" SO","").replace(u" OT", "").split("-")]
+            if scores[0] > scores[1]:
+                assigned_outcome = (outcome[0], outcome[1], scores)
+            else:
+                assigned_outcome = (outcome[1], outcome[0], scores)
+
+        league.add_fixture((result[1], result[2], assigned_outcome))
+
+    start = time.time()
+    progress = Progress()
+    finals = []
+    range_max = 1000000
+    for i in range(0, range_max):
+        league.reset()
+        finals.append(league.play_games())
+
+        #if i % 1000 == 0:
+        progress.progress_bar(range_max, i, "Simulating ")
+    end = time.time()
+    team_standings = {}
+
+    for final in finals:
+        for i, team in enumerate(final):
+            if i < 8:
+                addition = 1
+            else:
+                addition = 0
+            try:
+                team_standings[team] += addition
+            except KeyError:
+                team_standings[team] = addition
+
+    for team, total in team_standings.items():
+        print "%s, %d, %f%%" % (team.name, len(team.outcomes), (total * 1.0/range_max * 1.0) * 100.0)
+
+
+    print "%d in %f" % (range_max, end-start)
+    #for f in final:
+    #    print f.name, f.points, len(f.outcomes)
+
+
+
+
+
